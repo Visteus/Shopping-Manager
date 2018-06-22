818a466081921eb6a73b32fa24f5d349e1423daa
Tuan Pham <tuanvpham32@outlook.com>
2018-06-21 22:28:00 -0400

    update views with login

--- web-app/shoppingmanager/core/views.py
+++ web-app/shoppingmanager/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.template import loader
 
-def index(request):
-	return HttpResponse("Test")
+def login_view(request):
+	return render(request, 'login.html')
