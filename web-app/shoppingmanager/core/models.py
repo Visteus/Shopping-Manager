84fdd52c24f772ab990998900dbf0b012d8fac3f
dduranvz <dduranvz@gmail.com>
2018-06-25 21:10:20 -0400

    Transactions Table -JD- 06/25/18

--- web-app/shoppingmanager/core/models.py
+++ web-app/shoppingmanager/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# User model same as contact manager?
+
+# Simple table with 5 columns.
+# One User to many Transactions.
+# Deletes all transactions associated with a User when that User gets deleted.
+class Transactions(models.Model):
+	name = models.CharField(max_length=50)
+	total = models.DecimalField(max_digits=None, decimal_places=2)
+	time = models.DateTimeField(auto_now_add=True)
+	userKey = models.ForeignKey(User, on_delete=models.CASCADE)
