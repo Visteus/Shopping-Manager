85a01c3ce416a3f63af2a4ab74ea24401f361817
Tuan Pham <tuanvpham32@outlook.com>
2018-06-26 22:29:51 -0400

    correct fields for model

--- web-app/shoppingmanager/core/models.py
+++ web-app/shoppingmanager/core/models.py
@@ -6,8 +6,11 @@ from django.contrib.auth.models import User
 # Simple table with 5 columns.
 # One User to many Transactions.
 # Deletes all transactions associated with a User when that User gets deleted.
-class Transactions(models.Model):
-	name = models.CharField(max_length=50)
+class Transaction(models.Model):
+	title = models.CharField(max_length=50)
 	total = models.DecimalField(max_digits=12, decimal_places=2)
 	time = models.DateTimeField(auto_now_add=True)
-	userKey = models.ForeignKey(User, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "%s" % (self.title)
