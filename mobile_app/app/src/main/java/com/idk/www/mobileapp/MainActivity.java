4a71f41b92ccb1ba1b9b7cd4dbdb8bfe12088c72
DAVID\David <sixiongy248k@knights.ucf.edu>
2018-07-05 02:01:01 -0400

    Design alphav2.0 wip!

---
+++ mobile_app/app/src/main/java/com/idk/www/mobileapp/MainActivity.java
@@ -0,0 +1,119 @@
+package com.idk.www.mobileapp;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+   // @BindView(R.id.input_name) EditText nameinput;
+   // @BindView(R.id.input_email) EditText emailinput;
+   // @BindView(R.id.input_psw) EditText pswinput;
+  //  @BindView(R.id.input_signup) EditText signupinput;
+   // @BindView(R.id.input_login) EditText logininput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //signupinput.setOnClickListener(new View.OnClickListener(){
+           // @Override
+            //public void onClick(View v) {
+                //lead to another page with actual signup and actual signup button
+              //  signup();
+           // }
+        //});
+
+        //logininput.setOnClickListener(new View.OnClickListener(){
+           // @Override
+            //public void onClick(View v) {
+               // login();
+            //}
+        //});
+    }
+
+    /*
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+     public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+    */
+
+
+
+}
