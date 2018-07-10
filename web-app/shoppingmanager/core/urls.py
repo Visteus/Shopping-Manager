83880347f9e8996843e8af6c221455f7dc6e87f4
Tuan Pham <tuanvpham32@outlook.com>
2018-07-09 22:11:56 -0400

    update dashboard

--- web-app/shoppingmanager/core/urls.py
+++ web-app/shoppingmanager/core/urls.py
@@ -6,4 +6,7 @@ app_name = 'core'
 
 urlpatterns = [
 	path('', views.login_view, name='login_view'),
+	path('dashboard/', views.dashboard_view, name='dashboard_view'),
+	# path('signup/', views.sign_up, name='sign_in'),
+	path('logout/', views.logout_view, name='logout_view')
 ]
--- web-app/shoppingmanager/core/views.py
+++ web-app/shoppingmanager/core/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404, render_to_response
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Transaction
+from django.db.models import Q
 
 def login_view(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse('core:dashboard_view'))
+		else:
+			return render(
+				request,
+				'login.html',
+				{
+					'error_message': "You are not logged in. Please log in!"
+				}
+			)
 	return render(request, 'login.html')
+
+
+@login_required(login_url='')
+def dashboard_view(request):
+	user_id = request.user.id
+	transactions_list = Transaction.objects.filter(user_id=user_id)
+	return render(
+		request,
+		'dashboard.html',
+		{
+			'transactions_list': transactions_list,
+			'user': request.user
+		}
+	)
+
+
+@login_required(login_url='')
+def logout_view(request):
+	logout(request)
+	return render(request, 'logout.html')
